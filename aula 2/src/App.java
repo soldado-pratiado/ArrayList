92c4128363d876df1d402a6c4f2f1275961fc7e2
soldado-pratiado <pereira.oliveira.gustavo1610@escola.pr.gov.br>
2026-04-16 09:10:49 -0300

    Update App.java

--- aula 2/src/App.java
+++ aula 2/src/App.java
@@ -9,7 +9,7 @@ public class App {
         Integer opcao = 0;
         while(opcao != 6){
            
-            System.out.println("Este é um CRUD de alunos! \nDigite 1 para cadastrar\nDigite 2 para mostrar\nDigite 3 para deletar");
+            System.out.println("Este é um CRUD de alunos! \nDigite 1 para cadastrar\nDigite 2 para mostrar\nDigite 3 para deletar\nDigite 4 para editar");
             opcao = sc.nextInt();
             switch (opcao) {
                 case 1:
@@ -70,25 +70,25 @@ public class App {
         Scanner sc = new Scanner(System.in);
         Scanner sc2 = new Scanner(System.in);
         Aluno aluno = alunos.get(id);
-        System.out.println("O nome do aluno é: "+aluno.getNome()+"digite 1 para editar: ");
+        System.out.println("O nome do aluno é: "+aluno.getNome()+" digite 1 para editar: ");
         Integer opcao = sc.nextInt();
         if(opcao == 1){
             System.out.println("Digite o novo nome: ");
             aluno.setNome(sc2.nextLine());
         }
-        System.out.println("A idade do aluno é: "+aluno.getIdade()+"digite 1 para editar: ");
+        System.out.println("A idade do aluno é: "+aluno.getIdade()+" digite 1 para editar: ");
         opcao = sc.nextInt();
         if(opcao ==1){
             System.out.println("Digite a nova idade: ");
             aluno.setIdade(sc.nextInt());
         }
-        System.out.println("O curso do aluno é: "+aluno.getCurso()+"digite 1 para editar: ");
+        System.out.println("O curso do aluno é: "+aluno.getCurso()+" digite 1 para editar: ");
         opcao = sc.nextInt();
         if(opcao ==1){
             System.out.println("Digite o novo curso: ");
             aluno.setCurso(sc2.nextLine());
         }
-        System.out.println("A turma do aluno é: "+aluno.getTurma()+"digite 1 para editar: ");
+        System.out.println("A turma do aluno é: "+aluno.getTurma()+" digite 1 para editar: ");
         opcao = sc.nextInt();
         if(opcao ==1){
             System.out.println("Digite o novo turma: ");
